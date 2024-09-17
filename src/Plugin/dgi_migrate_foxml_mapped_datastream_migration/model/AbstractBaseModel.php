34be65e993db9d906ca1b4a8b60c19662977f72f
Adam Vessey <adam@discoverygarden.ca>
2024-09-17 13:30:29 -0300

    Log missing media type/bundle and skip missing media use terms.

--- src/Plugin/dgi_migrate_foxml_mapped_datastream_migration/model/AbstractBaseModel.php
+++ src/Plugin/dgi_migrate_foxml_mapped_datastream_migration/model/AbstractBaseModel.php
@@ -2,9 +2,11 @@
 
 namespace Drupal\dgi_migrate_foxml_mapped_datastream_migration\Plugin\dgi_migrate_foxml_mapped_datastream_migration\model;
 
+use Drupal\Core\Entity\EntityTypeBundleInfoInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\Core\Plugin\PluginBase;
 use Drupal\dgi_migrate_foxml_mapped_datastream_migration\ModelInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -20,10 +22,37 @@ abstract class AbstractBaseModel extends PluginBase implements ModelInterface, C
   protected array $mapping;
 
   /**
+   * Entity type bundle info service.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeBundleInfoInterface
+   */
+  protected EntityTypeBundleInfoInterface $entityTypeBundleInfo;
+
+  /**
+   * Logger service.
+   *
+   * @var \Psr\Log\LoggerInterface
+   */
+  protected LoggerInterface $logger;
+
+  /**
    * {@inheritDoc}
    */
   public function getMapping(): array {
-    return $this->mapping ??= $this->doGetMapping();
+    $media_type_exists = function ($mapping, $key) {
+      $exists = isset($this->entityTypeBundleInfo->getBundleInfo('media')[$mapping['media_type']]);
+
+      if (!$exists) {
+        $this->logger->warning('Media type "{type}" does not exist; skipping mapping of "{datastream_id}" for "{model_plugin_id}".', [
+          'type' => $mapping['media_type'],
+          'datastream_id' => $key,
+          'model_plugin_id' => $this->getPluginId(),
+        ]);
+      }
+
+      return $exists;
+    };
+    return $this->mapping ??= array_filter($this->doGetMapping(), $media_type_exists, ARRAY_FILTER_USE_BOTH);
   }
 
   /**
@@ -40,7 +69,12 @@ abstract class AbstractBaseModel extends PluginBase implements ModelInterface, C
    * {@inheritDoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) : static {
-    return new static($configuration, $plugin_id, $plugin_definition);
+    $instance = new static($configuration, $plugin_id, $plugin_definition);
+
+    $instance->entityTypeBundleInfo = $container->get('entity_type.bundle.info');
+    $instance->logger = $container->get('logger.channel.dgi_migrate_foxml_mapped_datastream_migration');
+
+    return $instance;
   }
 
 }
