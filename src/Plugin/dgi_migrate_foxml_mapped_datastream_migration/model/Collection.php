a5f465b9767703f3eae6cd61c9bba59449d036e4
Adam Vessey <adam@discoverygarden.ca>
2025-12-17 11:21:27 -0400

    Fix namespacing.
    Silly PHPStorm. (I should look into fixing whatever configuration, some time).

--- src/Plugin/dgi_migrate_foxml_mapped_datastream_migration/model/Collection.php
+++ src/Plugin/dgi_migrate_foxml_mapped_datastream_migration/model/Collection.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace Drupal\dgi_migrate_foxml_mapped_datastream_migration\src\Plugin\dgi_migrate_foxml_mapped_datastream_migration\model;
+namespace Drupal\dgi_migrate_foxml_mapped_datastream_migration\Plugin\dgi_migrate_foxml_mapped_datastream_migration\model;
 
 use Drupal\dgi_migrate_foxml_mapped_datastream_migration\Attribute\Model;
-use Drupal\dgi_migrate_foxml_mapped_datastream_migration\Plugin\dgi_migrate_foxml_mapped_datastream_migration\model\AbstractThumbnailedModel;
 
 /**
  * Collection content model.
